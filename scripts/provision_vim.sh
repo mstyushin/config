729573462d60b05279c13a24dff33a4cfab2d81f
Maxim Styushin <makcimkos@gmail.com>
2021-10-06 18:38:43 +0400

    install nerdtree

--- scripts/provision_vim.sh
+++ scripts/provision_vim.sh
@@ -6,6 +6,7 @@ VIMHOME="${HOME}/.vim"
 # plugin versions go here
 VIMGO_V="v1.25"
 PYTHONMODE_V="0.13.0"
+NERDTREE_V="6.10.16"
 
 mkdir -p ${VIMHOME}/pack/plugins/start ${VIMHOME}/pack/plugins/opt ${VIMHOME}/colors
 
@@ -23,6 +24,17 @@ fi
 if [[ ! -d "${VIMHOME}/pack/plugins/start/python-mode" ]]; then
     git clone --recurse-submodules git@github.com:python-mode/python-mode.git ${VIMHOME}/pack/plugins/start/python-mode
     cd ${VIMHOME}/pack/plugins/start/python-mode && git checkout ${PYTHONMODE_V}
+fi
+
+# install nerdtree plugin
+if [[ ! -d "${VIMHOME}/pack/plugins/start/nerdtree" ]]; then
+    git clone git@github.com:preservim/nerdtree.git ${VIMHOME}/pack/plugins/start/nerdtree
+    cd ${VIMHOME}/pack/plugins/start/nerdtree && git checkout ${NERDTREE_V}
+fi
+
+# install nerdtree plugin
+if [[ ! -d "${VIMHOME}/pack/plugins/start/nerdtree-git-plugin" ]]; then
+    git clone git@github.com:Xuyuanp/nerdtree-git-plugin.git ${VIMHOME}/pack/plugins/start/nerdtree-git-plugin 
 fi
 
 # rebuild helptags for everything just in case
