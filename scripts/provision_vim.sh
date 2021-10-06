6d1e6a47b6fbfcd0dd0ba082dc2a96323b14d582
Maxim Styushin <makcimkos@gmail.com>
2021-10-06 16:02:38 +0400

    install python-mode plugin

--- scripts/provision_vim.sh
+++ scripts/provision_vim.sh
@@ -3,6 +3,10 @@
 WORKDIR=$( cd $( dirname ${BASH_SOURCE[0]})/../ && pwd )
 VIMHOME="${HOME}/.vim"
 
+# plugin versions go here
+VIMGO_V="v1.25"
+PYTHONMODE_V="0.13.0"
+
 mkdir -p ${VIMHOME}/pack/plugins/start ${VIMHOME}/pack/plugins/opt ${VIMHOME}/colors
 
 # install color theme
@@ -10,11 +14,19 @@ cp ${WORKDIR}/vim/colors/* ${VIMHOME}/colors/
 
 # install vim-go plugin
 if [[ ! -d "${VIMHOME}/pack/plugins/start/vim-go" ]]; then
-  git clone git@github.com:fatih/vim-go.git ${VIMHOME}/pack/plugins/start/vim-go
-  cd ${VIMHOME}/pack/plugins/start/vim-go && git checkout v1.25
-  vim -c 'GoInstallBinaries' +qall
+    git clone git@github.com:fatih/vim-go.git ${VIMHOME}/pack/plugins/start/vim-go
+    cd ${VIMHOME}/pack/plugins/start/vim-go && git checkout ${VIMGO_V}
+    vim -c 'GoInstallBinaries' +qall
 fi
 
+# install python-mode plugin
+if [[ ! -d "${VIMHOME}/pack/plugins/start/python-mode" ]]; then
+    git clone --recurse-submodules git@github.com:python-mode/python-mode.git ${VIMHOME}/pack/plugins/start/python-mode
+    cd ${VIMHOME}/pack/plugins/start/python-mode && git checkout ${PYTHONMODE_V}
+fi
+
+# rebuild helptags for everything just in case
+vim -c 'helptags ALL' +qall
 
 echo "vim has been provisioned"
 
