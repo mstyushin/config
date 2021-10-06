ddc0ce2e341fba4fb9e2da66d783c55ad158863f
Maxim Styushin <makcimkos@gmail.com>
2021-10-06 12:36:20 +0400

    use v1.25 for vim-go

--- scripts/provision_vim.sh
+++ scripts/provision_vim.sh
@@ -4,8 +4,9 @@ mkdir -p ${HOME}/.vim/pack/plugins/start ${HOME}/.vim/pack/plugins/opt
 
 # install vim-go plugin
 git clone git@github.com:fatih/vim-go.git ${HOME}/.vim/pack/plugins/start/vim-go
+cd ${HOME}/.vim/pack/plugins/start/vim-go && git checkout v1.25
 
-vim -c 'GoInstallBinaries" +qall
+vim -c 'GoInstallBinaries' +qall
 
 echo "vim has been provisioned"
 
